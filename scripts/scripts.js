688c1c0e5d1867ce5e773456e3febbd6eccffc07
Tina Shabstari <tshabestari@my.bcit.ca>
2021-05-07 21:41:31 -0700

    Add a weight tracker session using vanilla javascript format and just styleing to make into the center and the call.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -62,6 +62,15 @@ function showbody(e){
         }
     }
 }
+document.getElementById("output").style.visibility="hidden";
+document.getElementById("lbsInput").addEventListener('input',
+   function(e){
+    document.getElementById("output").style.visibility ="visible";
+    let lbs = e.target.value;
+    document.getElementById('poundOutput').innerHTML=lbs *1
+	document.getElementById('kgOutput').innerHTML = lbs /2.2046
+});
+
 
 addbtn.addEventListener("click", save);
 addbtn.addEventListener("click", populatelist);
