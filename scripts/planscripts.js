fe447e7aaca70e7e1d09af302736489a7d9d463e
Bryan Xu <xubryan123@gmail.com>
2021-05-13 16:54:23 -0700

    split script pages

---
+++ scripts/planscripts.js
@@ -0,0 +1,59 @@
+/*dark mode function: 
+-create dark mode class within css 
+-set variable to body element using document.body
+-toggle dark mode class using element.classList.toggle()
+
+hide buttons function: 
+-use js to access the button using document.getElementById()
+-set the button to change display property to none onclick using .style.display = 'none'
+
+save button function:
+-use console.log to prompt user to enter title of note
+-add object to object array
+-return title name in variable
+-select nav bar using document.getElementById()
+-create new element using document.createElement() using title name variable
+-add to nav using .appendChild()*/
+
+const textarea1 = document.querySelector(".text-area");
+const list = document.querySelector(".weight");
+const addbtn = document.querySelector(".blue_button");
+const unitWeight = document.querySelector(".weight-toggle");
+const eplan = document.querySelector(".plan");
+const planbtn = document.querySelector(".plan_button");
+
+let planlist = [{title: '', body: ''}];
+
+function save(wlist){
+    var newtitle = prompt("Please enter the date: ");
+    wlist.push({title: newtitle, body: textarea1.value});
+}
+
+
+function populatelist(loc) {
+    loc.innerHTML= '';
+    if (loc === eplan){
+        for (let item of planlist) {
+            let elem = document.createElement("li");
+            let text = document.createTextNode(item.title);
+            elem.appendChild(text);
+            loc.appendChild(elem);
+        }
+    }
+}
+
+function showbodyplan(e){
+    if (e.target !== e.currentTarget){
+        var clickedNote = e.target.textContent;
+        for (let item of planlist) {
+            if (item.title === clickedNote){
+                textarea1.value = item.body;
+            }
+        }
+    }
+}
+
+planbtn.addEventListener("click", save.bind(null, planlist), false);
+planbtn.addEventListener("click", populatelist.bind(null, eplan), false);
+eplan.addEventListener("click", showbodyplan, false);
+
