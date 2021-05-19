0aab88bc1489333202287982da6e23d0826aa33a
Bryan Xu <xubryan123@gmail.com>
2021-05-18 17:23:18 -0700

    connected to database

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const sqlite3 = require('sqlite3').verbose()
 
 // static files
 app.use(express.static('public'))
 app.use('/css', express.static(__dirname + 'public/css'))
 app.use('/js', express.static(__dirname + 'public/js'))
 app.use('/images', express.static(__dirname + 'public/images'))
+
+//database object
+let db = new sqlite3.Database('C:/Users/xubry_000/desktop/ACIT2911/Agile_Development_Group_22/db/test.db', sqlite3.OPEN_READWRITE, (err) => {
+    if (err) {
+        console.error(err.message);
+    }
+    else{
+        console.log('Connected to the database.');
+    }
+})
 
 //display ejs
 app.set('views', './views')
