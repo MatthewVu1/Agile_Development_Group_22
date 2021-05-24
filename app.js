5c9655e90f168dc50bbabb9d3ff32d6559716d75
Bryan Xu <xubryan123@gmail.com>
2021-05-24 13:24:24 -0700

    implemented introduction video to node.js

--- app.js
+++ app.js
@@ -9,16 +9,6 @@ app.use('/css', express.static(__dirname + 'public/css'))
 app.use('/js', express.static(__dirname + 'public/js'))
 app.use('/images', express.static(__dirname + 'public/images'))
 
-// // database object
-// let db = new sqlite3.Database('db/test.db', sqlite3.OPEN_READWRITE, (err) => {
-//     if (err) {
-//         console.error(err.message);
-//     }
-//     else{
-//         console.log('Connected to the database.');
-//     }
-// })
-
 //sqlite connect
 let db = new sqlite3.Database('./db/test.db', sqlite3.OPEN_READWRITE, (err) => {
     if (err) {
@@ -32,19 +22,6 @@ app.set('views', './views')
 app.set('view engine', 'ejs')
 
 //display html
-// app.get('/', (req, res) =>{
-//     // res.render('yi_index.ejs')
-//     let sql = `SELECT Date date FROM WeightTrack
-//     ORDER BY EntryID`;
-
-//     db.all(sql, [], (err, rows) => {
-//         if (err) {
-//           throw err;
-//         }
-//         res.render('yi_index.ejs', { _data : rows});
-//         });
-//       });
-
 app.get('', (req, res) =>{
   let sql = `SELECT EntryID entryid, Date date, Weight weight, Unit unit FROM WeightTrack
   ORDER BY EntryID`;
@@ -70,6 +47,18 @@ app.get('/yi_index.ejs', (req, res) =>{
         });
       });
 
+app.get('/plans.ejs', (req, res) =>{
+  res.render('plans.ejs')
+})
+    
+app.get('/running.ejs', (req, res) =>{
+  res.render('running.ejs')
+})
+    
+app.get('/login.ejs', (req, res) =>{
+  res.render('login.ejs')
+})
+    
 
 var appjs = require('./public/js/weightscripts')
 console.log(appjs.newEntry());
@@ -124,13 +113,6 @@ app.post("/delete", (req, res) => {
   });
 });
 
-app.get('/plans.ejs', (req, res) =>{
-    res.render('plans.ejs')
-})
-
-app.get('/running.ejs', (req, res) =>{
-  res.render('running.ejs')
-})
 
 // Listen on port
 app.listen(port, () => console.log(`Listening on port ` + port))
