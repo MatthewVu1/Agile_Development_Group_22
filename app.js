f81c8ba1eaa48ae6f15703249d4ffa66163f2813
Yi <ydong27@bcit.ca>
2021-05-26 11:49:03 -0700

    commit on sql_integrate

--- app.js
+++ app.js
@@ -69,6 +69,7 @@ app.use(express.urlencoded({
   extended: true
 }))
 
+// WEIGHT
 // insert
 app.post("/new", (req, res) => {
   var newent = req.body.weight;
@@ -111,9 +112,62 @@ app.post("/delete", (req, res) => {
   });
 });
 
+// PLAN
+
 app.get('/plans.ejs', (req, res) =>{
-    res.render('plans.ejs')
-})
+  let sql = `SELECT EntryID entryid, Title title, Plan plan FROM CustomPlan
+    ORDER BY EntryID`;
+
+    db.all(sql, [], (err, rows) => {
+      if (err) {
+        throw err;
+      }
+      res.render('plans.ejs', { plans : rows});
+      });
+    });
+
+// insert
+app.post("/newplan", (req, res) => {
+  var newent = req.body.plan;
+  var newdate = req.body.title;
+  console.log(newent)
+  console.log(newdate)
+  const sql = `INSERT INTO CustomPlan(Title, Plan) VALUES (?, ?)`
+
+  db.run(sql, [newdate, newent], function(err) {
+    if (err) {
+      return console.log(err.message);
+    }
+    res.redirect("/plans.ejs");
+});
+});
+
+// update
+app.post("/editplan", (req, res) => {
+  const id = req.body.id;
+  var newent = req.body.plan;
+  const sql = 'UPDATE CustomPlan SET Plan = ? WHERE EntryID = ?'
+
+  db.run(sql, [newent, id], function(err) {
+    if (err) {
+      return console.log(err.message);
+    }
+    res.redirect("/plans.ejs");
+  });
+});
+
+// delete
+app.post("/deleteplan", (req, res) => {
+  const id = req.body.id;
+  const sql = 'DELETE FROM CustomPlan WHERE EntryID = ?'
+  db.run(sql, id,function(err) {
+    if (err) {
+      return console.error(err.message);
+    }
+    res.redirect("/plans.ejs");
+  });
+});
+
 
 // Listen on port
 app.listen(port, () => console.log(`Listening on port ` + port))
